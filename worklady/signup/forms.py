82e141f86a008abcd6eaf07fed5f717fdfc6e3d9
Suri <quffl2002@naver.com>
2024-07-01 21:22:52 +0900

    비번찾기X 소셜로그인X 구현

---
+++ worklady/signup/forms.py
@@ -0,0 +1,68 @@
+from django import forms
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.http import HttpResponseBadRequest, JsonResponse
+from .models import CustomUser
+
+User = CustomUser
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise forms.ValidationError('Invalid username or password.')
+
+        return cleaned_data
+
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(label='비밀번호 확인', widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'birthday', 'password', 'password_confirm', 'phone_number')
+        widgets = {
+            'password': forms.PasswordInput(),
+            'password_confirm': forms.PasswordInput(),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("비밀번호가 일치하지 않습니다.")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+def login_page(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                # 로그인 성공
+                login(request, user)
+                return redirect('home')
+            else:
+                # 로그인 실패
+                print("로그인 실패")
+    else:
+        form = LoginForm()
+    return render(request, 'Login.html', {'form': form})
---
+++ worklady/signup/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from .views import signup, signup_success, join_page, login_page, main_page, home_page,privacyClause,workladyClause, findemail,send_verification_email, verify_code
+from .views import UserActivateView
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path('signup/', signup, name = 'signup'),
+    path('signup/success', signup_success, name = 'signup_success'),
+    path('join/',join_page, name='join'),
+    path('login/',login_page, name='login'),
+    path('main/', main_page, name='main'),
+    path('home/', home_page, name='home'),
+    path('privacyClause/', privacyClause, name='privacyClause'),
+    path('workladyClause/', workladyClause, name='workladyClause'),
+    path('findemail/', findemail, name='findemail'),
+    path('activate/<str:uid>/<str:token>/', UserActivateView.as_view(), name='activate'),
+    path('send_verification_email/', send_verification_email, name='send_verification_email'),
+    path('verify_code/', verify_code, name='verify_code'),
+    #path('find_pw/',views.PwResetEmailSendView.as_view()),
+    #path('pwchange/<str:uid>/<str:token>',views.PasswordChangeView.as_view(), name ='pwchange'),
+    #path('pwchange/<str:uid>/<str:token>/', views.PasswordChangeView.as_view(), name='pwchange'),
+]
