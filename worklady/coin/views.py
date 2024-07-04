a8db26309f44863cd9fb308417b84435b76bb3cd
Suri <quffl2002@naver.com>
2024-07-04 14:41:49 +0900

    결제 제외 코인 구현 완료

--- worklady/coin/views.py
+++ worklady/coin/views.py
@@ -26,6 +26,10 @@ def coin_history(request):
 
 @login_required
 def coin_purchase(request):
+    return render(request, 'coinUp.html')
+
+@login_required
+def process_coin_purchase(request):
     if request.method == 'POST':
         coin_option = request.POST.get('coin_option')
         payment_method = request.POST.get('payment_method')
@@ -44,7 +48,7 @@ def coin_purchase(request):
                 amount=coin_amount,
             )
             return redirect('coin_history')
-    return render(request, 'coinUp.html')
+    return redirect('coin_purchase')
 
 @login_required
 def cash_conversion(request):
@@ -67,3 +71,25 @@ def cash_conversion(request):
 
             return render(request, 'conversion_complete.html', {'cash_amount': cash_amount, 'account_holder': account_holder, 'bank_name': bank_name, 'account_number': account_number})
     return render(request, 'coinToMoney.html')
+
+@login_required
+def process_cash_conversion(request):
+    if request.method == 'POST':
+        account_holder = request.POST.get('account_holder')
+        bank_name = request.POST.get('bank_name')
+        account_number = request.POST.get('account_number')
+        
+        user_coins = Coin.objects.filter(user=request.user).first()
+        if user_coins and user_coins.amount > 0:
+            cash_amount = user_coins.amount * 500
+            # 모든 코인을 환전
+            CoinTransaction.objects.create(
+                user=request.user,
+                transaction_type='사용',
+                amount=-user_coins.amount
+            )
+            user_coins.amount = 0
+            user_coins.save()
+
+            return render(request, 'conversion_complete.html', {'cash_amount': cash_amount, 'account_holder': account_holder, 'bank_name': bank_name, 'account_number': account_number})
+    return redirect('cash_conversion')
